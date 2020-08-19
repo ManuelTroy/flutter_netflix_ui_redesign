a7e12c34f2e0ac1e21747a48803507fded42482b
ManuelTroy <69712652+ManuelTroy@users.noreply.github.com>
2020-08-18 20:52:58 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Netflix UI Redesign',
+      title: 'Netflix UI Redesign',
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
